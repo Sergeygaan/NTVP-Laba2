e9ad97932369a3f5e21e27221b64d857c0e94fb8
sergeygaan <sergeygaan@mail.ru>
2015-11-19 12:49:08 +0600

    readability

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -23,54 +23,40 @@ namespace WindowsFormsApplication1
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e){}
 
-        }
-
-          private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
+        private void textBox1_TextChanged(object sender, EventArgs e){}
 
         // Расчет почасам
         private void button1_Click(object sender, EventArgs e)
         {
             IPayroll Horly = new Hourly();
-
             int clock = Convert.ToInt32(textBox1.Text);
             int payment = Convert.ToInt32(textBox2.Text);
             int one = 1;
-
             label4.Text = Horly.Earnings(clock, payment, one).ToString();
         }
         // Расчет по окладу
         private void button2_Click(object sender, EventArgs e)
         {
             IPayroll Salary = new Salary();
-
             float ratemonth = Convert.ToInt32(textBox3.Text);
             int rateday = Convert.ToInt32(numericUpDown1.Text);
             int dayworked = Convert.ToInt32(numericUpDown2.Text);
-
             if (dayworked > rateday)
             {
                 numericUpDown2.Text = rateday.ToString();
                 dayworked = rateday;
-
             }
-    
             label10.Text = Salary.Earnings(ratemonth, rateday, dayworked).ToString();
         }
         // Расчет по  ставке
         private void button3_Click(object sender, EventArgs e)
         {
             IPayroll Rate = new Rate();
-
             int change = Convert.ToInt32(textBox5.Text);
             int cost = Convert.ToInt32(textBox4.Text);
             int one = 1;
-
             label12.Text = Rate.Earnings(change, cost, one).ToString();
         }
 
